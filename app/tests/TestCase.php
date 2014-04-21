c7a7b4cc03e257061b1928642d648903cf9f4498
Leonardo Graterol <pankas87@gmail.com>
2014-04-21 01:18:18 -0300

    TestCase, setup method: Calling migrate and seed command

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -3,6 +3,16 @@
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 	/**
+	* Default preparation for each test
+	*
+	*/
+	public function setup()
+	{
+		parent::setUp();
+		$this->prepareForTests();
+	}
+
+	/**
 	 * Creates the application.
 	 *
 	 * @return \Symfony\Component\HttpKernel\HttpKernelInterface
@@ -16,4 +26,14 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
 
+	/**
+	* Method that performs preparation for each test
+	*
+	* @return void
+	*/
+	public function prepareForTests()
+	{
+		Artisan::call('migrate');		
+		$this->seed();
+	}
 }
