81990ea94e34f54c21752cd10e9115fddd74c2fa
Leonardo Graterol <pankas87@gmail.com>
2014-04-21 18:25:06 -0300

    CoursedateUserStatusModelTest skeleton

---
+++ app/tests/unit/CoursedateUserStatusModelTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class CoursedateUserStatusModelTest extends TestCase {
+
+	public function testCreate()
+	{
+
+	}
+
+	public function testRead()
+	{
+
+	}
+
+	public function testUpdate()
+	{
+
+	}
+
+	public function testDelete()
+	{
+
+	}
+}
