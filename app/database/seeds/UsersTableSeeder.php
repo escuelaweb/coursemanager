20d704ad02521ec817ce0fbd56b7678594f6b86a
Leonardo Graterol <pankas87@gmail.com>
2014-04-21 17:31:44 -0300

    UsersTableSeeder: Adding valid RUTs to newly created users

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -18,12 +18,25 @@ class UsersTableSeeder extends Seeder {
 
 	$faker = Faker::create();
 
-	foreach(range(1, 10) as $index) {
+	$ruts = array(
+		'23845761-0',
+		'23845762-9',
+		'23845763-7',
+		'23845764-5',
+		'23845765-3',
+		'23845766-1',
+		'23845767-K',
+		'23845768-8',
+		'23845769-6',
+		'23845770-K'
+	);
+
+	foreach(range(1, 10) as $key => $index) {
 		User::create([
 			'first_name'						=> $faker->firstName,
 			'last_name'							=> $faker->lastName,
 			'email'									=> $faker->email,
-			'rut'										=> '30686957-X',
+			'rut'										=> $ruts[$key],
 			'password'							=> '12345678',
 			'birthday'							=> $faker->date('Y-m-d', '-10 years')
 		]);
