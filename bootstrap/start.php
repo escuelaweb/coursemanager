a3b948738d4757882f7f0b59a87e7f56f6eaf65a
Leonardo Graterol <pankas87@gmail.com>
2014-05-11 23:04:41 -0400

    Adding a new machine name in the local environment detection array

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('leonardo-VirtualBox'),
+	'local' => array('leonardo-VirtualBox', 'el-hierro'),
 
 ));
 
